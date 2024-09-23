f2baac134abfc56ce035c68439cc8c5294ca1351
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-09-23 17:21:13 +0200

    feat: add migration and seed database table with fake data

---
+++ database/seeders/ProjectsSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Project;
+use Faker\Generator as Faker;
+use App\Functions\Helper;
+
+class ProjectsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        for ($i = 0; $i < 100; $i++) {
+            $new_project = new Project();
+            $new_project->title = $faker->sentence(6);
+            $new_project->slug = Helper::generateSlug($new_project->title, Project::class);
+            $new_project->image = $faker->imageUrl(640,480, 'projects', true);
+            $new_project->date = $faker->date('Y-m-d');
+            $new_project->description = $faker->paragraph(4);
+            $new_project->save();
+        }
+    }
+}
